b1a79f155732a0af80070d1a9507541d4cb457cc
ujwlkarn <ujwlkarn@gmail.com>
2014-06-28 12:47:02 +0530

    1

---
+++ extract.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+import requests
+
+url = raw_input("Enter a website to extract the URL's from: ")
+
+r  = requests.get("http://" +url)
+
+data = r.text
+
+soup = BeautifulSoup(data)
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
